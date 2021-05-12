167e754cfba928c69bb941cc86b7925ea68288c3
Krish Tech Studio <69856755+krishtechstudio@users.noreply.github.com>
2021-05-12 17:06:50 +0530

    Update ERC20Detailed.sol

--- ERC20Detailed.sol
+++ ERC20Detailed.sol
@@ -9,16 +9,17 @@ contract ERC20Detailed is IERC20 {
     string private _name;
     string private _symbol;
     uint8 private _decimals;
-
+    uint256 private _minimumSupply;
     /**
      * @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
      * these values are immutable: they can only be set once during
      * construction.
      */
-    constructor (string memory name, string memory symbol, uint8 decimals) public {
+    constructor (string memory name, string memory symbol, uint8 decimals, unit256 minimumSupply) public {
         _name = name;
         _symbol = symbol;
         _decimals = decimals;
+        _minimumSupply = minimumSupply;
     }
 
     /**
