8ccd19d39c1a2436702b664065835b392f2cda2a
Krish Tech Studio <69856755+krishtechstudio@users.noreply.github.com>
2021-05-12 19:55:08 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -17,6 +17,6 @@ contract Token is ERC20, ERC20Detailed {
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
     constructor () public ERC20Detailed("SecureMint", "SMT", 18) {
-        _mint(msg.sender, 10000000000000 * (10 ** uint256(decimals())), 1000000 * (10 ** unit256(decimals())));
+        _mint(msg.sender, 10000000000000000 * (10 ** uint256(decimals())), 1000000 * (10 ** unit256(decimals())));
     }
 }
